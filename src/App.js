55948d135e95a9f6f763833acfa51570d6cd4190
syed Hussain <syed.hussain@cerner.com>
2023-03-13 09:11:31 +0530

    create

--- src/App.js
+++ src/App.js
@@ -6,65 +6,60 @@ import axios from 'axios'
 import { useEffect } from "react";
 function App() {
    
-  const [employees,setEmployees]=useState([])
-  const [count,setCount] =useState(0)
-  const [name,setName]=useState(null)
+  const [firstName,setFirstName]=useState("")
+  const [lastName,setLastName]=useState("")
+  const [email,setEmail]=useState("")
+  const [phone,setPhone]=useState("")
+  const [hireDate,setHireDate]=useState()
+  const [jobTitle,setJobTitle]=useState("")
+  const [salary,setSalary]=useState("")
+  const [managerId,setManagerId]=useState("")
+const [message,setMessage]=useState("") 
+  const handleSubmit =() =>{
+   axios.post('http://localhost:8080/reactemployees',{
+first_name:firstName,
+last_name:lastName,
+email:email,
+phone:phone,
+hire_date:hireDate,
+job_title:jobTitle,
+salary:salary,
+manager_id:managerId
 
-  useEffect(()=>{
-   axios.get('http://localhost:8080/reactemployees',{headers:{
+   },{headers:{
 'Authorization':'Basic bWljcm9jYXJlOjEyM2FBYmM='
    }}).then(
      resp =>
 
      {
-      console.log(resp.data) 
-      setEmployees(resp.data)}
+      console.log(resp.status) 
+      setMessage(resp.data)
+      }
    )
 
     
-    })
+    }
 
 
   return (
-    <div className="App">
-     <div className="container" align="center">
-<div className="d-flex justify-content-end">
-<input type="text" id="myInput"  placeholder="Search for data.." /> 
-  <form method="post"> 
-         <input type="submit" value="Sign Out" className='btn btn-success'/> 
-         </form>
-         </div>
-<table id="myTable" className="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Employee Id</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-
-    </tr>
-  </thead>
-  <tbody>
-   {employees.map((employee,index) =>
-   (
-
-    <tr key={employee.employee_id}>
-      <td>{employee.employee_id}</td>
-      <td>{employee.first_name}</td>
-      <td>{employee.last_name}</td>
-      <td>{employee.email}</td>
-      <td>{employee.phone}</td>
-    </tr>
-   )
-    )}
-   
-  </tbody>
-</table>
-
-
-</div>
-<a href="/employees">go to employees</a>
+    <div className="container">
+     
+        <h1>Employee Management System</h1>
+        <h1>{message}</h1>
+        <form id="createemp" action="#" onSubmit={handleSubmit}>
+            <input id="first_name" onChange={(e) => setFirstName(e.target.value)} type="text"  placeholder="First Name" className="form-control mb-4 col-4" required/> 
+            <input id="last_name" onChange={(e) => setLastName(e.target.value)} type="text"  placeholder="Last Name" className="form-control mb-4 col-4" required/>
+            <input id="email" type="text"  onChange={(e) => setEmail(e.target.value)} placeholder="Email" className="form-control mb-4 col-4" required/>
+            <input id="phone" type="text"  onChange={(e) => setPhone(e.target.value)} placeholder="Phone" className="form-control mb-4 col-4" required/>
+            <input id="hire_date" type="date"  onChange={(e) => setHireDate(e.target.value)} placeholder="Hire Date" className="form-control mb-4 col-4" required/>
+            <input id="job_title" type="text"  onChange={(e) => setJobTitle(e.target.value)} placeholder="Job Title" className="form-control mb-4 col-4" required/>
+            <input id="salary" type="text"  onChange={(e) => setSalary(e.target.value)} placeholder="Salary" className="form-control mb-4 col-4" required/>
+            <input id="manager_id" type="text"  onChange={(e) => setManagerId(e.target.value)} placeholder="Manager Id" className="form-control mb-4 col-4" required/>
+           
+            <button type="submit" id="button" className="btn btn-info col-2"  >create</button>
+        </form>
+ 
+        <a href="/employees"> Back to Employee List</a>
     </div>
   );
 }
